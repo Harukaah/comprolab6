f2f6cf0235907d5f34aba8bf0a6d5587b4c2cc1b
Haruka <sirapob_yon@cmu.ac.th>
2023-12-01 15:18:42 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,33 @@
 using namespace std;
 
 int main(){
+    int num;
+    int even = 0;
+    int odd = 0;
+    
+    
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> num;
+    if (num % 2 == 0 && num != 0) {
+                even++; }
+            else if (num %2 != 0 && num !=0) {
+                odd++; }
+            else {
+             }
+    
+    while (num != 0) {
+            cout << "Enter an integer: ";
+            cin >> num;
+            if (num % 2 == 0 && num != 0) {
+                even++; }
+            else if (num %2 != 0 && num !=0) {
+                odd++; }
+            else {
+            break; }
+                
+    }
+        
+    cout << "#Even numbers = " << even << endl;
+    cout << "#Odd numbers = " << odd;
     return 0;
 }
